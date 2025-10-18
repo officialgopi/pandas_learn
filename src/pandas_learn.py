f834f64707b9ba23485c1ea5b8760b2d9f8d6649
OfficialGopi <official.gopi@outlook.com>
2025-10-18 15:51:12 +0530

    init_4

--- src/pandas_learn.py
+++ src/pandas_learn.py
@@ -9,6 +9,12 @@ def use_pandas(data):
     print(data.tail(10)) # initial last 5 rows
     # access a column
     print(data['PassengerId'])
+    # Series is a one-dimensional array-like structure in Pandas that can hold a sequence of value of any data type. Each value in a Series is associated with an index label, which allows for easy access and manipulation of the data.Custom index labels can be assigned to a Series during its creation or modified later.
+    print(type(data['PassengerId'])) # <class 'pandas.core.series.Series'>
+
+    #create a series
+    series_scalar = pd.Series(10,index = ['a','b','c' ,'d','e','f','g','h','i','j'])
+    print(series_scalar)
 
 
 def main():

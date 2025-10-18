5249acf6474031d8b927ef87b6a1074944d1c661
OfficialGopi <official.gopi@outlook.com>
2025-10-18 15:38:50 +0530

    init_2

--- src/pandas_learn.py
+++ src/pandas_learn.py
@@ -1,11 +1,24 @@
 import pandas as pd
 
-# Load the Titanic dataset
-data = pd.read_csv('./dataset/titanic.csv')
 
-# print(data)
+def use_pandas(data):
+    # Print the first few rows
+    print(data.head(10)) #initial 5 rows
+    print(type(data)) # <class 'pandas.core.frame.DataFrame'>
+    # Print the last  few rows
+    print(data.tail(10)) # initial last 5 rows
 
-print(data.head())
 
-print(type(data))
+def main():
+    try:
+        # Load the Titanic dataset  
+        data = pd.read_csv('./dataset/titanic.csv')
+        use_pandas(data)
+    except FileNotFoundError:
+        print("The dataset file was not found. Please ensure the path is correct.")
+    # print(data)
+
+
+
+main()
 
